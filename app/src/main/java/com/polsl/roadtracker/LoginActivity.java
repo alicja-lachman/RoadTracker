47333527a01b2689bed39be6f6902a6be825bb88
WPaczula <wojciech.paczula@gmail.com>
2017-03-16 23:07:31 +0100

    Extend login activity

--- app/src/main/java/com/polsl/roadtracker/LoginActivity.java
+++ app/src/main/java/com/polsl/roadtracker/LoginActivity.java
@@ -2,25 +2,29 @@ package com.polsl.roadtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import butterknife.BindView;
 
 public class LoginActivity extends AppCompatActivity {
     private Toast message;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
 
-    public void onLoginButtonClick() {
+    public void onLoginButtonClick(View v) {
         if(message!=null)
             message.cancel();
+
         message = Toast.makeText(this, "Login", Toast.LENGTH_SHORT);
         message.show();
     }
 
-    public void onRegisterClick() {
+    public void onRegisterClick(View v) {
         if(message!=null)
             message.cancel();
         message = Toast.makeText(this, "Register", Toast.LENGTH_SHORT);
