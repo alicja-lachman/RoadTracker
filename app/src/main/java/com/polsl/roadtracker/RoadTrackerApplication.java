c2c5503f39580d4c50f24c7f172c529816667655
alicja-lachman <alicja.lachman@gmail.com>
2017-03-29 18:52:13 +0200

    Added and configured GreenDao. Prepared entity models. Created UploadStatus enum and db converter

--- app/src/main/java/com/polsl/roadtracker/RoadTrackerApplication.java
+++ app/src/main/java/com/polsl/roadtracker/RoadTrackerApplication.java
@@ -2,6 +2,11 @@ package com.polsl.roadtracker;
 
 import android.app.Application;
 
+import com.polsl.roadtracker.database.entity.DaoMaster;
+import com.polsl.roadtracker.database.entity.DaoSession;
+
+import org.greenrobot.greendao.database.Database;
+
 import timber.log.Timber;
 
 /**
@@ -9,9 +14,19 @@ import timber.log.Timber;
  */
 
 public class RoadTrackerApplication extends Application {
+    public static final boolean ENCRYPTED = false;
+
+    private DaoSession daoSession;
     @Override
     public void onCreate() {
         super.onCreate();
-        Timber.plant(new Timber.DebugTree()); //necessary to use timber
+        Timber.plant(new Timber.DebugTree());
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(this, ENCRYPTED ? "notes-db-encrypted" : "notes-db");
+        Database db = ENCRYPTED ? helper.getEncryptedWritableDb("super-secret") : helper.getWritableDb();
+        daoSession = new DaoMaster(db).newSession();
+    }
+
+    public DaoSession getDaoSession() {
+        return daoSession;
     }
 }
---
+++ app/src/main/java/com/polsl/roadtracker/database/entity/RouteData.java
@@ -0,0 +1,266 @@
+package com.polsl.roadtracker.database.entity;
+
+import com.polsl.roadtracker.database.UploadStatus;
+import com.polsl.roadtracker.database.UploadStatusPropertyConverter;
+
+import org.greenrobot.greendao.DaoException;
+import org.greenrobot.greendao.annotation.Convert;
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Generated;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.JoinProperty;
+import org.greenrobot.greendao.annotation.OrderBy;
+import org.greenrobot.greendao.annotation.ToMany;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by alachman on 29.03.2017.
+ */
+@Entity
+public class RouteData {
+    @Id(autoincrement = true)
+    private Long id;
+    private Date startDate;
+    private Date endDate;
+    private String description;
+
+    @ToMany(joinProperties = {
+            @JoinProperty(name = "id", referencedName = "routeId")
+    })
+    @OrderBy("timestamp ASC")
+    private List<AccelometerData> accelometerDataList;
+
+    @ToMany(joinProperties = {
+            @JoinProperty(name = "id", referencedName = "routeId")
+    })
+    @OrderBy("timestamp ASC")
+    private List<LocationData> locationDataList;
+
+    @ToMany(joinProperties = {
+            @JoinProperty(name = "id", referencedName = "routeId")
+    })
+    @OrderBy("timestamp ASC")
+    private List<GyroscopeData> gyroscopeDataList;
+
+
+    @Convert(converter = UploadStatusPropertyConverter.class, columnType = Integer.class)
+    private UploadStatus uploadStatus;
+    /**
+     * Used to resolve relations
+     */
+    @Generated(hash = 2040040024)
+    private transient DaoSession daoSession;
+    /**
+     * Used for active entity operations.
+     */
+    @Generated(hash = 368698595)
+    private transient RouteDataDao myDao;
+
+
+    @Generated(hash = 1668166539)
+    public RouteData(Long id, Date startDate, Date endDate, String description,
+                     UploadStatus uploadStatus) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.uploadStatus = uploadStatus;
+    }
+
+    @Generated(hash = 280729267)
+    public RouteData() {
+    }
+
+    public void start() {
+        setStartDate(new Date(System.currentTimeMillis()));
+        setUploadStatus(UploadStatus.NOT_UPLOADED);
+        setDescription("My route number " + this.getId());
+    }
+
+    public void finish() {
+        setEndDate(new Date(System.currentTimeMillis()));
+        setUploadStatus(UploadStatus.READY_TO_UPLOAD);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UploadStatus getUploadStatus() {
+        return this.uploadStatus;
+    }
+
+    public void setUploadStatus(UploadStatus uploadStatus) {
+        this.uploadStatus = uploadStatus;
+    }
+
+    /**
+     * To-many relationship, resolved on first access (and after reset).
+     * Changes to to-many relations are not persisted, make changes to the target entity.
+     */
+    @Generated(hash = 226986899)
+    public List<AccelometerData> getAccelometerDataList() {
+        if (accelometerDataList == null) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            AccelometerDataDao targetDao = daoSession.getAccelometerDataDao();
+            List<AccelometerData> accelometerDataListNew = targetDao
+                    ._queryRouteData_AccelometerDataList(id);
+            synchronized (this) {
+                if (accelometerDataList == null) {
+                    accelometerDataList = accelometerDataListNew;
+                }
+            }
+        }
+        return accelometerDataList;
+    }
+
+    /**
+     * Resets a to-many relationship, making the next get call to query for a fresh result.
+     */
+    @Generated(hash = 186320036)
+    public synchronized void resetAccelometerDataList() {
+        accelometerDataList = null;
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 128553479)
+    public void delete() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.delete(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 1942392019)
+    public void refresh() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.refresh(this);
+    }
+
+    /**
+     * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
+     * Entity must attached to an entity context.
+     */
+    @Generated(hash = 713229351)
+    public void update() {
+        if (myDao == null) {
+            throw new DaoException("Entity is detached from DAO context");
+        }
+        myDao.update(this);
+    }
+
+    /**
+     * called by internal mechanisms, do not call yourself.
+     */
+    @Generated(hash = 884091940)
+    public void __setDaoSession(DaoSession daoSession) {
+        this.daoSession = daoSession;
+        myDao = daoSession != null ? daoSession.getRouteDataDao() : null;
+    }
+
+    /**
+     * To-many relationship, resolved on first access (and after reset).
+     * Changes to to-many relations are not persisted, make changes to the target entity.
+     */
+    @Generated(hash = 1179144003)
+    public List<LocationData> getLocationDataList() {
+        if (locationDataList == null) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            LocationDataDao targetDao = daoSession.getLocationDataDao();
+            List<LocationData> locationDataListNew = targetDao
+                    ._queryRouteData_LocationDataList(id);
+            synchronized (this) {
+                if (locationDataList == null) {
+                    locationDataList = locationDataListNew;
+                }
+            }
+        }
+        return locationDataList;
+    }
+
+    /**
+     * Resets a to-many relationship, making the next get call to query for a fresh result.
+     */
+    @Generated(hash = 1180624715)
+    public synchronized void resetLocationDataList() {
+        locationDataList = null;
+    }
+
+    /**
+     * To-many relationship, resolved on first access (and after reset).
+     * Changes to to-many relations are not persisted, make changes to the target entity.
+     */
+    @Generated(hash = 1225420398)
+    public List<GyroscopeData> getGyroscopeDataList() {
+        if (gyroscopeDataList == null) {
+            final DaoSession daoSession = this.daoSession;
+            if (daoSession == null) {
+                throw new DaoException("Entity is detached from DAO context");
+            }
+            GyroscopeDataDao targetDao = daoSession.getGyroscopeDataDao();
+            List<GyroscopeData> gyroscopeDataListNew = targetDao
+                    ._queryRouteData_GyroscopeDataList(id);
+            synchronized (this) {
+                if (gyroscopeDataList == null) {
+                    gyroscopeDataList = gyroscopeDataListNew;
+                }
+            }
+        }
+        return gyroscopeDataList;
+    }
+
+    /**
+     * Resets a to-many relationship, making the next get call to query for a fresh result.
+     */
+    @Generated(hash = 1166767524)
+    public synchronized void resetGyroscopeDataList() {
+        gyroscopeDataList = null;
+    }
+
+}
+
