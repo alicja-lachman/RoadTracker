11bef5cdf116939149cc57edc009dda631472ded
rafaswo046 <rafaswo046@student.polsl.pl>
2017-04-16 00:44:13 +0200

    Added location reading and adding it to database, probably

--- app/src/main/java/com/polsl/roadtracker/activity/LoginActivity.java
+++ app/src/main/java/com/polsl/roadtracker/activity/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.polsl.roadtracker.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
@@ -20,15 +24,16 @@ import com.polsl.roadtracker.util.KeyboardHelper;
 import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
-    private Toast message;
-    private String login = "login";
-    private String password = "password";
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     @BindView(R.id.et_login)
     EditText etLogin;
     @BindView(R.id.et_password)
     EditText etPassword;
     @BindView(R.id.activity_login)
     LinearLayout parentView;
+    private Toast message;
+    private String login = "login";
+    private String password = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,8 +41,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         KeyboardHelper.setupUI(parentView, this);
+        checkLocationPermission();
     }
-
 
     public void onLoginButtonClick(View v) {
 
@@ -53,6 +58,38 @@ public class LoginActivity extends AppCompatActivity {
     public void onRegisterClick(View v) {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
-        overridePendingTransition( R.anim.slide_in_right, R.anim.slide_out_right );
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_right);
+    }
+
+    //TODO: if no permission, stop application because it can't work properly
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user asynchronously -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- app/src/main/java/com/polsl/roadtracker/activity/MainActivity.java
+++ app/src/main/java/com/polsl/roadtracker/activity/MainActivity.java
@@ -9,8 +9,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.LocationListener;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -20,14 +22,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
 import com.polsl.roadtracker.SensorReader;
 import com.polsl.roadtracker.R;
 import com.polsl.roadtracker.dagger.di.component.DaggerDatabaseComponent;
 import com.polsl.roadtracker.dagger.di.component.DatabaseComponent;
 import com.polsl.roadtracker.dagger.di.module.DatabaseModule;
+import com.polsl.roadtracker.database.entity.LocationData;
+import com.polsl.roadtracker.database.entity.LocationDataDao;
 import com.polsl.roadtracker.database.entity.RouteData;
 import com.polsl.roadtracker.database.entity.RouteDataDao;
-import com.polsl.roadtracker.utility.LocationReader;
+
 
 
 import javax.inject.Inject;
@@ -35,17 +47,25 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+import static com.polsl.roadtracker.activity.LoginActivity.MY_PERMISSIONS_REQUEST_LOCATION;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     @BindView(R.id.start_stop_button)
     Button actionButton;
     @Inject
     RouteDataDao routeDataDao;
+    @Inject
+    LocationDataDao locationDataDao;
     private Toast message;
     private RouteData route;
     private DatabaseComponent databaseComponent;
     private SensorReader sensorReader;
     private LocationManager locationManager;
-    private LocationReader locationReader;
+    private LocationRequest mLocationRequest;
+    private LocationSettingsRequest.Builder builder;
+    private GoogleApiClient googleApiClient;
+    private Location mCurrentLocation;
+    private Long timestamp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,13 +75,55 @@ public class MainActivity extends AppCompatActivity {
         injectDependencies();
         if (sensorReader == null)
             sensorReader = new SensorReader((SensorManager) getSystemService(SENSOR_SERVICE));
-        if (locationReader == null) {
-            locationReader = new LocationReader();
-        }
         if (locationManager == null) {
             locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         }
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        createLocationRequest();
+        createBuilder();
+        updateValuesFromBundle(savedInstanceState);
+    }
 
+    //TODO: will be useful when working in background, to save the state of activity and restore button state
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            // Update the value of mRequestingLocationUpdates from the Bundle, and
+            // make sure that the Start Updates and Stop Updates buttons are
+            // correctly enabled or disabled.
+            //if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+            //    mRequestingLocationUpdates = savedInstanceState.getBoolean(
+            //            REQUESTING_LOCATION_UPDATES_KEY);
+            //    setButtonsEnabledState();
+           // }
+
+            // Update the value of mCurrentLocation from the Bundle and update the
+            // UI to show the correct latitude and longitude.
+            //if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                // mCurrentLocationis not null.
+                //mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            //}
+
+            // Update the value of mLastUpdateTime from the Bundle and update the UI.
+            //if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+               // mLastUpdateTime = savedInstanceState.getString(
+                 //       LAST_UPDATED_TIME_STRING_KEY);
+            //}
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        //savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+        //        mRequestingLocationUpdates);
+        //savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        //savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+       // super.onSaveInstanceState(savedInstanceState);
     }
 
     public void onStartButtonClick(View v) {
@@ -69,21 +131,9 @@ public class MainActivity extends AppCompatActivity {
             actionButton.setText("END");
             route = new RouteData();
             route.start();
-            locationReader.setId(route.getId());
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, locationReader);
-            sensorReader.startSensorReading(route.getId());
-
             routeDataDao.insert(route);
+            startLocationUpdates();
+            sensorReader.startSensorReading(route.getId());
         } else {
             new AlertDialog.Builder(this)
                     .setTitle(R.string.ending_trace_route)
@@ -92,10 +142,10 @@ public class MainActivity extends AppCompatActivity {
                         public void onClick(DialogInterface dialog, int which) {
                             actionButton.setText("START");
                             sensorReader.finishSensorReadings();
-                            locationManager.removeUpdates(locationReader);
+                            stopLocationUpdates();
                             route.finish();
                             routeDataDao.update(route);
-                            //TODO more stuff - example saving our road into local database
+                            //TODO: save actual state of button(if already clicked start)
                         }
                     })
                     .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
@@ -105,7 +155,6 @@ public class MainActivity extends AppCompatActivity {
                     })
                     .setIcon(android.R.drawable.ic_dialog_info)
                     .show();
-
         }
     }
 
@@ -142,14 +191,103 @@ public class MainActivity extends AppCompatActivity {
         databaseComponent.inject(this);
     }
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void createBuilder() {
+        builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient,
+                        builder.build());
+    }
+
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
+        //resume location updates when back at activity
+        //if (googleApiClient.isConnected() && !mRequestingLocationUpdates) {
+        //    startLocationUpdates();
+        //}
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        //stop location updates when out of activity
+        //stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                googleApiClient, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        timestamp = System.currentTimeMillis();
+        if (mCurrentLocation != null) {
+            double longitude = mCurrentLocation.getLongitude();
+            double latitude = mCurrentLocation.getLatitude();
+            LocationData locationData = new LocationData(timestamp,latitude,longitude,route.getId());
+            locationDataDao.insert(locationData);
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: not really needed, cause it's at login activity
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an expanation to the user asynchronously -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                googleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
 
