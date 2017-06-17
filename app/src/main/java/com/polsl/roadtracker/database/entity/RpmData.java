58d506efcb8564f350a6b9694273e56253fce8ef
alicja-lachman <alicja.lachman@gmail.com>
2017-06-17 22:47:25 +0200

    Fixed typos. Cleaning. Changed OBD units from imperial to metric.

--- app/src/main/java/com/polsl/roadtracker/database/entity/RpmData.java
+++ app/src/main/java/com/polsl/roadtracker/database/entity/RpmData.java
@@ -8,22 +8,26 @@ import org.greenrobot.greendao.annotation.Index;
  * Created by Jakub on 15.05.2017.
  */
 @Entity
-public class RmpData {
+public class RpmData {
     @Index
     private Long timestamp;
     private float value;
 
 
-    @Generated(hash = 316504271)
-    public RmpData(Long timestamp, float value) {
+
+
+    @Generated(hash = 434139780)
+    public RpmData(Long timestamp, float value) {
         this.timestamp = timestamp;
         this.value = value;
     }
 
-    @Generated(hash = 978505827)
-    public RmpData() {
+    @Generated(hash = 1398823423)
+    public RpmData() {
     }
-    
+
+
+ 
 
     public Long getTimestamp() {
         return timestamp;
